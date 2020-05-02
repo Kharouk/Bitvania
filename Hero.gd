5c239fda5c47b009f8def7c49ac2b83498a081c8
Alex Kharouk <akharoukpp@gmail.com>
2020-05-02 08:31:14 +0100

    polish up sloping

--- Hero.gd
+++ Hero.gd
@@ -15,12 +15,14 @@ export (int) var MAX_SLOPE_ANGLE = 46
 var motion = Vector2.ZERO
 
 var snap_vector := Vector2.DOWN * 4
+var has_just_jumped : bool = false
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
 # similar to _process but for physics based movement
 func _physics_process(delta):
+	has_just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(delta, input_vector)
 	apply_friction(input_vector)
@@ -50,13 +52,14 @@ func apply_friction(input_vector):
 
 func update_snap_vector():
 	if is_on_floor():
-		snap_vector = Vector2.DOWN * 4
+		snap_vector = Vector2.DOWN
 		
 
 func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			has_just_jumped = true
 			snap_vector = Vector2.ZERO
 	else:
 		# allows us to cancel the jump fully and jump shorter (not entirely clear on the second conditional but if we remove it then the player can jump as much as they want.. which sounds cool for a power up)
@@ -64,9 +67,10 @@ func jump_check():
 			motion.y = -JUMP_FORCE/2
 			
 func apply_gravity(delta):
-	motion.y += GRAVITY * delta
-	# prevents us from falling faster than our jump force:
-	motion.y = min(motion.y, JUMP_FORCE)
+	if not is_on_floor():
+		motion.y += GRAVITY * delta
+		# prevents us from falling faster than our jump force:
+		motion.y = min(motion.y, JUMP_FORCE)
 
 func update_animations(input_vector):
 	if input_vector.x != 0:
@@ -82,8 +86,30 @@ func update_animations(input_vector):
 		
 
 func move_hero():
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_position = position
+	var last_motion = motion
+	var snap_force = 4
+	
 	# detects collision with other bodies; allows you to keep moving against the other body as you "slide"
 	# move and slide already takes into account delta time
 	# we tell move_and_slide_with_snap the motion, our snap vector towards the floor, where is the floor (pointing up), that we want to snap to slope, 4 max slides, and the 45 degree to radian
-	motion = move_and_slide_with_snap(motion, snap_vector, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+	motion = move_and_slide_with_snap(motion, snap_vector * snap_force, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+	
+	# Landing
+	if was_in_air and is_on_floor():
+		# If we are landing on a slope, we keep our previous momentum; no awkward stopping
+		motion.x = last_motion.x
 
+	# Just left ground:
+	if was_on_floor and !is_on_floor() and not has_just_jumped:
+		# prevents it from hopping the player when going up a slope 
+		motion.y = 0
+		position.y = last_position.y
+	
+	# Prevent sliding (hack)
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 1:
+		# if we are on a slope, that is not moving, and we are sliding just a little bit, then set our position to the position we are at when we first touch the slope
+		position.x = last_position.x
+	

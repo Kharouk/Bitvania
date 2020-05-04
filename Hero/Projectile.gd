9d01f7d4672874f7197f3469dda8d31fdb4b6e27
Alex Kharouk <akharoukpp@gmail.com>
2020-05-04 19:53:20 +0100

    ignore basic warnings around unused variables

--- Hero/Projectile.gd
+++ Hero/Projectile.gd
@@ -5,5 +5,6 @@ var velocity = Vector2.ZERO
 func _process(delta):
 	position += velocity * delta
 
+# warning-ignore:unused_argument -> ignore unused viewport var
 func _on_Visibility_viewport_exited(viewport):
 	queue_free()

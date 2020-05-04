042fb740147c8a7e693236b90f2bf3ee2bb09e50
Alex Kharouk <akharoukpp@gmail.com>
2020-05-04 06:46:28 +0100

    comments

--- Hero/PlayerBullet.gd
+++ Hero/PlayerBullet.gd
@@ -3,4 +3,5 @@ extends "res://Hero/Projectile.gd"
 func _ready():
 	# stops the muzzle flash from following the bullet, stops the process of the parent Projectile
 	# once animation starts it sets process back to true
+	# don't run the process until animation occurs
 	set_process(false)

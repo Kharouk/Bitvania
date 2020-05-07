ef44e5a4402d4be5f756ab01f83806022433817e
Alex Kharouk <akharoukpp@gmail.com>
2020-05-07 07:14:25 +0100

    set up enemy stats and life system so that enemies can die after a certain amount of bullets

---
+++ Enemies/EnemyStats.gd
@@ -0,0 +1,14 @@
+extends Node
+
+signal enemy_died
+
+export(int) var max_health = 1
+
+# needs to be onready since we are using that export variable above since we could change the variable in the gui
+onready var health = max_health setget set_health
+
+func set_health(value):
+	health = clamp(value, 0, max_health)
+	print(health, " -> health")
+	if health == 0:
+		emit_signal("enemy_died")
--- Hero/Projectile.gd
+++ Hero/Projectile.gd
@@ -12,10 +12,10 @@ func _on_Visibility_viewport_exited(viewport):
 	queue_free()
 
 
-func _on_Hitbox_body_entered(body):
+func _on_Hitbox_body_entered(_body):
 	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
 
-func _on_Hitbox_area_entered(area):
+func _on_Hitbox_area_entered(_area):
 	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()

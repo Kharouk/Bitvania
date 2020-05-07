bdc5134e7566c99f4067263f65294b50fdc6d72e
Alex Kharouk <akharoukpp@gmail.com>
2020-05-07 06:41:49 +0100

    attach the animation effects to jumping and when bullet hits

--- Hero/Projectile.gd
+++ Hero/Projectile.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const ExplosionEffect = preload("res://Effects/ExplosionEffect.tscn")
+
 var velocity = Vector2.ZERO
 
 func _process(delta):
@@ -11,7 +13,9 @@ func _on_Visibility_viewport_exited(viewport):
 
 
 func _on_Hitbox_body_entered(body):
+	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
 
 func _on_Hitbox_area_entered(area):
+	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()

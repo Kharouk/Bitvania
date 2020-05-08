b2d53b26aee56c7412374949d38e48e383d29199
Alex Kharouk <akharoukpp@gmail.com>
2020-05-08 12:49:20 +0100

    fix errors

--- Hero/PlayerStats.gd
+++ Hero/PlayerStats.gd
@@ -7,6 +7,6 @@ var health = max_health setget set_health
 signal player_died
 
 func set_health(value):
-  health = clamp(value, 0, max_health)
-  if health == 0:
-    emit_signal("player_died")
+	health = clamp(value, 0, max_health)
+	if health == 0:
+		emit_signal("player_died")

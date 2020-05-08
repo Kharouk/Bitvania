9a08b713dd18e0293821d98b85bdba1312ae912f
Alex Kharouk <akharoukpp@gmail.com>
2020-05-08 12:56:18 +0100

    set up logic for screen shake

--- World/Camera.gd
+++ World/Camera.gd
@@ -2,5 +2,19 @@ extends Camera2D
 
 var shake = 0
 
+onready var timer = $Timer
+
 func _ready():
 	pass
+
+func _process(delta):
+	offset_h = rand_range(-shake, shake)
+	offset_v = rand_range(-shake, shake)
+
+func screen_shake(amount, duration):
+	shake = amount
+	timer.wait_time = duration
+	timer.start()
+
+func _on_Timer_timeout():
+	shake = 0

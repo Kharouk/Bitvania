2ac89c3d3f5c445d306acf94825a5fa74f83b93b
Alex Kharouk <akharoukpp@gmail.com>
2020-05-08 14:04:01 +0100

    fix bug where health doesn't drop the last health

--- UI/HealthMeter.gd
+++ UI/HealthMeter.gd
@@ -10,3 +10,7 @@ func _ready():
 func _on_player_health_changed(value):
   # 5 comes from it being 5 pixels per health bar
   full.rect_size.x = value * 5 + 1
+
+  # doesn't shrink less than 5 pixels so will have to remove to show empty health
+  if value <= 0:
+	  full.queue_free()

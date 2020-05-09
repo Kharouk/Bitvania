2a5f79a1934ee7179cbd6a82f56a8f2df03ff376
Alex Kharouk <akharoukpp@gmail.com>
2020-05-09 13:26:10 +0100

    set up double jump

--- Hero/Hero.gd
+++ Hero/Hero.gd
@@ -26,6 +26,7 @@ var motion = Vector2.ZERO
 
 var snap_vector := Vector2.DOWN
 var has_just_jumped : bool = false
+var double_jump := true
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
@@ -98,14 +99,22 @@ func update_snap_vector():
 func jump_check():
 	if is_on_floor() or coyoteJumpTimer.time_left > 0:
 		if Input.is_action_just_pressed("ui_up"):
-			Utils.instance_scene_on_main(JumpEffect, global_position)
-			motion.y = -JUMP_FORCE
+			jump(JUMP_FORCE)
 			has_just_jumped = true
-			snap_vector = Vector2.ZERO
 	else:
 		# allows us to cancel the jump fully and jump shorter (not entirely clear on the second conditional but if we remove it then the player can jump as much as they want.. which sounds cool for a power up)
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE/2
+		
+		if Input.is_action_just_pressed("ui_up") and double_jump:
+			jump(JUMP_FORCE * .75)
+			double_jump = false
+
+func jump(force):
+	Utils.instance_scene_on_main(JumpEffect, global_position)
+	motion.y = -force
+	snap_vector = Vector2.ZERO
+
 			
 func apply_gravity(delta):
 	if not is_on_floor():
@@ -145,6 +154,7 @@ func move_hero():
 		# If we are landing on a slope, we keep our previous momentum; no awkward stopping
 		motion.x = last_motion.x
 		Utils.instance_scene_on_main(JumpEffect, global_position)
+		double_jump = true
 
 	# Just left ground:
 	if was_on_floor and !is_on_floor() and not has_just_jumped:

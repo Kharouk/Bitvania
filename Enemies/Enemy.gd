1fe45c97b347266987cd9c8a8c8b8de8cc88504e
Alex Kharouk <akharoukpp@gmail.com>
2020-05-11 11:53:14 +0100

    add death effect to enemies and remove some useless enemies

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
 export (int) var MAX_SPEED = 15
 
 var motion = Vector2.ZERO
@@ -10,4 +12,5 @@ func _on_Hurtbox_hit(damage):
 
 
 func _on_EnemyStats_enemy_died():
+	Utils.instance_scene_on_main(EnemyDeathEffect, global_position)
 	queue_free()

de42b9d929afe2aef39bd9d03457f7e6d7317ef2
Alex Kharouk <akharoukpp@gmail.com>
2020-05-11 13:33:56 +0100

    Code up plant enemy's fire bullet

---
+++ Enemies/PlantEnemy.gd
@@ -0,0 +1,18 @@
+extends "res://Enemies/Enemy.gd"
+
+const EnemyBullet = preload("res://Enemies/Projectiles/EnemyBullet.tscn")
+
+export(int) var BULLET_SPEED = 30
+export(float) var SPREAD = 15
+
+onready var fireDirection = $FireDirection
+onready var bulletSpawnPoint = $BulletSpawnPoint
+
+func fire_bullet():
+  var bullet = Utils.instance_scene_on_main(EnemyBullet, bulletSpawnPoint.global_position)
+  print("position normalized: ", (fireDirection.global_position - global_position).normalized())
+  var velocity = (fireDirection.global_position - global_position).normalized() * BULLET_SPEED
+  print("velocity: ", velocity)
+
+  velocity = velocity.rotated(deg2rad(rand_range(-SPREAD, SPREAD)))
+  bullet.velocity = velocity

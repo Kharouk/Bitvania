75cf4354e4128d32fc86afb0c388ef280f8a30a6
Alex Kharouk <akharoukpp@gmail.com>
2020-05-13 20:07:22 +0100

    set up main file

---
+++ Enemies/Bitaloid.gd
@@ -0,0 +1,12 @@
+extends "res://Enemies/Enemy.gd"
+
+# Welcome, to the biggest, baddest enemy you've ever seen.
+
+func _ready():
+  pass
+
+func _physics_process(delta):
+  # this will probably be useful soon
+  if delta == "aah that is what delta is":
+    print("have a medal!")
+  pass

ab0ebc600c10bb1323f123a77f46b1e3a72700b7
Alex Kharouk <akharoukpp@gmail.com>
2020-05-13 07:09:30 +0100

    set up second level for connection

--- World/Levels/Door.gd
+++ World/Levels/Door.gd
@@ -8,5 +8,7 @@ export(String, FILE, "*.tscn") var new_level_path = ''
 var active = true
 
 func _on_Door_body_entered(Hero):
-  Hero.emit_signal("hit_door", self)
-  active = false
+  if active == true:
+    Hero.emit_signal("hit_door", self)
+    active = false
+    print('hello world')

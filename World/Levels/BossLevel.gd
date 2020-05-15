4a53f79191c272ed72e07072adb223ae611299c5
Alex Kharouk <akharoukpp@gmail.com>
2020-05-15 07:01:37 +0100

    Comeplete the boss level

--- World/Levels/BossLevel.gd
+++ World/Levels/BossLevel.gd
@@ -1,8 +1,17 @@
 extends "res://World/Levels/Level.gd"
 
+const PLAYER_BIT = 0
+
 onready var bitaloid = $Bitaloid
+onready var blockDoor = $BlockDoor
 
-
+func set_block_door(active):
+  blockDoor.visible = active
+  blockDoor.set_collision_mask_bit(PLAYER_BIT, active)
+  
 
 func _on_Trigger_area_triggered():
-	pass # Replace with function body.
+  set_block_door(true)
+  
+func _on_Bitaloid_died():
+  set_block_door(false)

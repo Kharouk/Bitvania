70771fa4d0ae56833b79a4a616a53d1f1059ea8e
Alex Kharouk <akharoukpp@gmail.com>
2020-05-15 07:22:55 +0100

    set up loading

--- SaverAndLoader.gd
+++ SaverAndLoader.gd
@@ -14,4 +14,19 @@ func save_game():
 
 
 func load_game():
-  return
+  var save_file = File.new()
+  if !save_file.file_exists("user://savegame.save"):
+    return
+
+  var persistingNodes = get_tree().get_nodes_in_group("Persists")
+  for node in persistingNodes:
+    node.queue_free()
+
+  save_file.open("user://savegame.save", File.READ)
+
+  while !save_file.eof_reached():
+    var current_line = parse_json(save_file.get_line())
+    if current_line != null:
+      var newNode = load(current_line["nodename"]).instance()
+      get_node(current_line["parent"]).add_child(newNode, true)
+      newNode.position = Vector2(current_line["position_x"], current_line["position_y"])

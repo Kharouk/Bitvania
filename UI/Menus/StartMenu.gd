061bbc9a8bf75a78e1e47a79845dca2c37a9828c
Alex Kharouk <akharoukpp@gmail.com>
2020-05-16 19:14:04 +0100

    incorporate loading with deletion if clicked on new

--- UI/Menus/StartMenu.gd
+++ UI/Menus/StartMenu.gd
@@ -5,13 +5,20 @@ func _ready():
 
 func _on_StartButton_pressed():
 	# warning-ignore:return_value_discarded
+	SaverAndLoader.delete_file()
 	get_tree().change_scene('res://World/World.tscn')
 	
 	
 func _on_LoadButton_pressed():
-	SaverAndLoader.is_loading = true
-	# warning-ignore:return_value_discarded
-	get_tree().change_scene('res://World/World.tscn')
+	if (SaverAndLoader.loaded_file):
+		SaverAndLoader.is_loading = true
+		# warning-ignore:return_value_discarded
+		get_tree().change_scene('res://World/World.tscn')
+	else:
+		print("new Game now") # TODO : set up alert saying that a new game has started
+		
+		get_tree().change_scene('res://World/World.tscn')
+		
 
 func _on_QuitButton_pressed():
 	get_tree().quit()

743ff12e289d819c194a6f83994016621f7fa719
Alex Kharouk <akharoukpp@gmail.com>
2020-05-19 10:05:26 +0100

    logic for loading game after death

--- UI/Menus/GameOverMenu.gd
+++ UI/Menus/GameOverMenu.gd
@@ -4,5 +4,8 @@ func _on_QuitButton_pressed():
 	get_tree().quit()
 
 
-func _on_LoadButton_pressed():
-	pass # Replace with function body.
+func _on_LoadButton_pressed() -> void:
+	SoundFX.play("Click", 1, -30)
+	SaverAndLoader.is_loading = false
+	Music.list_stop()
+	get_tree().change_scene("res://World/World.tscn")
